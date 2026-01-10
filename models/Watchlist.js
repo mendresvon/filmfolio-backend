fc7917696b1e65286219016bd54b8aac98c18aba
Von <mendresvon@gmail.com>
2026-01-10 21:41:56 +0800

    removed virtual functiom ._id->.id Want to stick with native mongodb language using ._id

--- models/Watchlist.js
+++ models/Watchlist.js
@@ -1,40 +1,30 @@
 const mongoose = require("mongoose");
 
-const WatchlistSchema = new mongoose.Schema(
-  {
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    name: {
-      type: String,
-      required: true,
-    },
-    description: {
-      type: String,
-    },
-    movies: [
-      {
-        movieId: Number,
-        movieTitle: String,
-        posterPath: String,
-        createdAt: { type: Date, default: Date.now },
-      },
-    ],
-    createdAt: {
-      type: Date,
-      default: Date.now,
-    },
+const WatchlistSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
   },
-  {
-    toJSON: { virtuals: true },
-    toObject: { virtuals: true },
-  }
-);
-
-WatchlistSchema.virtual("id").get(function () {
-  return this._id.toHexString();
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  movies: [
+    {
+      movieId: Number,
+      movieTitle: String,
+      posterPath: String,
+      createdAt: { type: Date, default: Date.now },
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports = mongoose.model("Watchlist", WatchlistSchema);
